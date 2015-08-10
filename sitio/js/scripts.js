206cdce728aa7ed0db06038eb3d23fec04c98e8e
entroid <hernansahha@gmail.com>
2015-08-10 09:13:47 -0300

    home bkg slide fixes

--- sitio/js/scripts.js
+++ sitio/js/scripts.js
@@ -1,31 +1,60 @@
-$(document).ready(function() {
-
-    /***************** Waypoints ******************/
-
-    $('.wp1').waypoint(function() {
-        $('.wp1').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
+/***************** Fancybox ******************/
+/*$(".youtube-media").on("click", function(e) {
+    var jWindow = $(window).width();
+    if (jWindow <= 768) {
+        return;
+    }
+    $.fancybox({
+        href: this.href,
+        padding: 4,
+        type: "iframe",
+        'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
     });
-    $('.wp2').waypoint(function() {
-        $('.wp2').addClass('animated fadeInDown');
-    }, {
-        offset: '75%'
-    });
-    $('.wp3').waypoint(function() {
-        $('.wp3').addClass('animated bounceInDown');
-    }, {
-        offset: '75%'
-    });
-    $('.wp4').waypoint(function() {
-        $('.wp4').addClass('animated fadeInDown');
-    }, {
-        offset: '75%'
-    });
+    return false;
+});*/
+var hideTxt = function () {
+    $('.slideme li h1').fadeOut();
+}
 
-    /***************** Flickity ******************/
+var txtEffect = function () {
+    $('.slideme li h1').fadeIn();
+}
 
-    $('#featuresSlider').flickity({
+var btnHover = function (btnSection) {
+    var slide;
+
+    if (btnSection === 'paucke') {
+        slide = 0;
+    }
+    if (btnSection === 'siglos') {
+        slide = 1;
+    }
+    if (btnSection === 'signos') {
+        slide = 2;
+    }
+    $('.hero').slideme('playTo', slide);
+    $('.hero').slideme('stop');
+}
+
+var startSlide = function () {
+    var btns = $('.home-cta .use-btn');
+
+    $(btns).mouseenter(function() {
+        console.log('enter')
+        var btnSection = $(this).attr('data-section');
+        btnHover(btnSection);
+    })
+
+    $(btns).mouseleave(function() {
+        console.log('out')
+        $('.hero').slideme('play');
+    })
+}
+
+function init () {
+
+/***************** Flickity ******************/
+    /*$('#featuresSlider').flickity({
         cellAlign: 'left',
         contain: true,
         prevNextButtons: false
@@ -36,47 +65,53 @@ $(document).ready(function() {
         contain: true,
         prevNextButtons: false,
         imagesLoaded: true
-    });
-
-    /***************** Fancybox ******************/
-
-    $(".youtube-media").on("click", function(e) {
-        var jWindow = $(window).width();
-        if (jWindow <= 768) {
-            return;
-        }
-        $.fancybox({
-            href: this.href,
-            padding: 4,
-            type: "iframe",
-            'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-        });
-        return false;
-    });
+    });*/
 
 
-    $("a.single_image").fancybox({
+/***************** Fancybox Init ******************/
+    /*$("a.single_image").fancybox({
         padding: 4,
-    });
+    });*/
 
 
-    /***************** Vegas (background slide) ******************/
-
-    $(".hero").vegas({
+/***************** Vegas (background slide) ******************/
+    /*$(".hero").vegas({
+        delay: 7000,
+        transition: 'flash',
+        transitionDuration: 2000,
+        cover: true,
         slides: [
-            { src: "img/01-screenshot.jpg" },
-            { src: "img/02-screenshot.jpg" },
-            { src: "img/03-screenshot.jpg" }
+            { src: "img/bkg/home-bckg-paucke.jpg" },
+            { src: "img/bkg/home-bckg-siglos.jpg" },
+            { src: "img/bkg/home-bckg-signos.jpg" }
         ],
 
         pause: function (index, slideSettings) {
             // body...
         }
-    })
+    })*/
 
-    $(".hero").on('vegasplay', function (e, index, slideSettings) {
-        console.log("Pause on Slide index " + index);
+/***************** Slide me (background slide) ******************/
+    $('.hero').slideme({
+        pagination: "numbers",
+        autoslide : true,
+        interval : 4000,
+        loop : true,
+        onCreatedCallback : startSlide, //init slide
+        onEndCallback : txtEffect, //end animaton
+        onStartCallback : hideTxt, //before start animation
+        speed : 200,        
+        /*resizable: {
+            width: 990,
+            height: 450,
+        }*/
+        transition : 'fade' // Values: 'fade/slide/page/zoom'
     });
+
+} 
+
+$(document).ready(function() {
+    init();
 });
 
 
