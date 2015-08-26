31ce30159a18fccfd33463da0eda14dd9b3c809a
Hernan Ainsa <hernan.ainsa@glb-d0759.(none)>
2015-08-26 19:10:45 -0300

    glb - boton mosaico

--- sitio/app/js/application/views/paucke/paucke.js
+++ sitio/app/js/application/views/paucke/paucke.js
@@ -12,10 +12,15 @@ define([
 
             events: {
                 'click #lightSlider li img': 'lightbox',
+                'click #grilla .hoverContainer *': 'stopProp',
+                'click #grilla .hoverContainer .hoverTitle': 'hoverLightbox',
+                'click #grilla .hoverContainer h3': 'hoverLightbox',
                 'click #lightSlider .hoverTitle *': 'stopProp',
                 'click #lightSlider .hoverTitle': 'hoverLightbox',
                 'click .navBook nav a': 'openTxt',
-                'click .cerrar': 'cerrar'
+                'click .cerrar': 'cerrar',
+                'click .grillaBtn': 'openGrilla',
+                'click .closeGrillaBtn': 'closeGrilla'
             },
 
             render: function() {
@@ -96,8 +101,9 @@ define([
             },
 
             hoverLightbox: function(e) {
+                console.log('entr')
                 var target = e.target;
-                var url = $(target).siblings('img').attr('data-src');
+                var url = $(target).parents('.hoverContainer').find('img').attr('data-src');
                 this.ajaxOverlay(url);
             },
             stopProp: function(e){
@@ -106,43 +112,67 @@ define([
 
             openTxt: function(e){
                 e.preventDefault();
+
                 var target = e.currentTarget,
                     seccion = $(target).attr('data-seccion'),
                     url = 'js/application/views/paucke/helpers/' + seccion + '.html',
                     element,
-                    este = this;                     
+                    este = this,
+                    cont = seccion === 'grilla' ? $('#grillaContent') : $('#txt');
 
+                $('#txt .txtArea').fadeOut();                    
+
+                if (seccion){
                     var ajaxCall = function(){
                         $.ajax({
                           url: url,
                           dataType: 'html',
                         }).done(function(data) {
                             var hacerVisible = function(){                             
-                                $('#txt').addClass('adentro');
+                                $(cont).addClass('adentro fondoAdentro');
                             }
 
-                            $('#txt').html(data);
+                            $(cont).html(data);
 
                             este.timeOut(hacerVisible, 100);
                                       
                             
                         }); 
                     }
+                }                
 
-                    $('#txt').removeClass('adentro');
+                $(cont).removeClass('adentro');
 
-                    este.timeOut(ajaxCall, 100);
+                este.timeOut(ajaxCall, 100);
             },
 
-            cerrar: function() {                
-                var hacerInvisible = function(){ 
+            cerrar: function(e, el) { 
+                var elemento = el ? el : '#txt';          
+                /*var hacerInvisible = function(){ 
                     $('#txt').removeClass('adentro')
                 }
-                this.timeOut(hacerInvisible, 100);
+                this.timeOut(hacerInvisible, 100);*/
+                $(elemento).removeClass('adentro fondoAdentro').html('');
+
             },
 
             timeOut: function(fn, temp) {
                 setTimeout(fn, temp);
+            },
+
+            openGrilla: function(e) {
+                this.cerrar(e, $('#txt'));
+                this.openTxt(e);
+                $('#txt').removeClass
+                $('.navBook .closeGrillaBtn').removeClass('hide');
+                $('.navBook .grillaBtn').addClass('hide');
+                
+            },
+            closeGrilla: function(e) {
+                this.cerrar(e, $('#txt'));
+                this.cerrar(e, $('#grillaContent'));
+                $('.navBook .closeGrillaBtn').addClass('hide');
+                $('.navBook .grillaBtn').removeClass('hide');
             }
         });
         
