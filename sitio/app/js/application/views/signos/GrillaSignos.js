e686ba4df18461a6723f17b10d8c30a7c27d06b2
Hernan Ainsa <hernan.ainsa@globant.com>
2015-10-29 18:35:17 -0300

    glb - signos mas fixes

--- sitio/app/js/application/views/signos/GrillaSignos.js
+++ sitio/app/js/application/views/signos/GrillaSignos.js
@@ -9,12 +9,24 @@ define([
             el: '.grillaSignos',
 
             events: {
-                /*'click #lightSlider li img': 'lightbox'*/
+                'mostrarImagen .signos_portada': 'mostrarImagenFun'
             },
 
             render: function() {
                 jsonData = this.model.get("jsonData")
                 $(this.el).html(_.template(grilla,{jsonData:jsonData}));
+                $(".signos_portada img").hide();
+                 $(".signos_portada img:eq(0)").show();
+            },
+
+            mostrarImagenFun:function(evt,el){
+            if($(el).prop("tagName")=="SPAN"){
+                $li = $(el).parent();
+            }else{
+                $li = $(el)
+            }
+            $(".signos_portada img").hide();
+             $(".signos_portada img:eq("+$li.data("index")+")").show();
             }
         });
         
